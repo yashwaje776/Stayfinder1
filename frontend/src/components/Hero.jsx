8fca9c83539d9d7a96c1578271b3244adb80c6b9
yashwaje776 <wajeyash6@gmail.com>
2025-06-20 12:13:25 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
 
   return (
     <div className='min-h-[70vh]'>
-      <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url('./src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center h-screen">
+      <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white  bg-no-repeat bg-cover bg-center h-screen" style={{ backgroundImage: `url(${assets.heroImage})` }}>
         <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">Stay Beyond Expectations</p>
         <h1 className="font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
           Discover Your Perfect Gateway Destination
